b1e5c7f07832370eca46e931f9b008ca5955be04
aml3ed <aml3ed@virginia.edu>
2017-10-16 21:06:42 -0400

    Lots oh changes

--- reflect.php
+++ reflect.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
 <head>
-    <title>Reflect</title>
+    <title>Plan</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Dancing+Script|Nunito:700|Open+Sans" rel="stylesheet">
+    <?php include reflection.php; ?>
+    <script type="text/javascript" src="backtoHome.js"></script>
 </head>
-<body>
-    <?php
+<body onload="backtoHome()">
+    <nav id="navbar">
+        <ul>
+            <li id="mainTitle"><a href="index.html"><img src="images/msLogoV1.png"  width="80px;" style="vertical-align: middle; margin-right: 5px;"/>MindSet</a></li>
+            <li><a href="breathe.html" class="navLink">Breathe</a></li>
+            <li><a href="plan.html" class="navLink">Plan</a></li>
+        </ul>
+    </nav>
+    <section id="main">
+        <h1>Reflect</h1>
+        <p id="quote">What you think, you become; what you feel, you attract; what you imagine, you create.</p>
+        <div class="container">
+            <form name="reflection" method="post">
+                <span class="error"><?php echo $error; ?></span>
+                <textarea rows="15" placeholder="What went well?" name="good"></textarea>
+                <textarea rows="15" placeholder="What could have gone better?" name="bad"></textarea>
+                <textarea rows="15" placeholder="What can I change?" name="ugly"></textarea>
+                <input type="submit" class="addBtn" value="Submit">
+            </form>
+            
+        </div>
         
-        $reflections = array(
-        
-        )
-    ?>
+    </section>
+    <section id="footer">
+        <p>Designed by Sarah Overton and Drew Lytle</p>
+    </section>
+    
 </body>
---
+++ reflection.php
@@ -0,0 +1,14 @@
+<?php
+// define variables and set to empty values
+$error = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["good"])) {
+    $err = "You must say what went well. ";
+  } else if(empty($_POST['bad'])) {
+      $err = $err + "You must say what could have gone better. "
+  } else if(empty($_POST['ugle'])){
+      $err = $err + "You must say what you could change. "
+  }
+}
+?>
